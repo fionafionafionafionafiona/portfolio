0fc387dec8682dbe9a83946b2a83252fee032b0a
fionaperina <fiona.perina@studentfr.ch>
2025-03-27 21:11:49 +0100

    intégration des projets 3D dans la page index.html - supression des pages de test

--- src/js/main.js
+++ src/js/main.js
@@ -6,11 +6,17 @@ var projects = [
     ratio: 1.16,
     url: "project.html",
   },
-  { img: require("../img/bleue.png"), ratio: 2, url: "project.html" },
-  // { img: "../img/forest.jpg", ratio: 0.6, url: "project.html" },
+  { img: require("../img/bleue.png"), ratio: 0.98, url: "project.html" },
+  { img: require("../img/forest.jpg"), ratio: 0.6, url: "project.html" },
+  { img: require("../img/vitamin-well.png"), ratio: 0.6, url: "project.html" },
+  { img: require("../img/qrace.png"), ratio: 0.79, url: "project.html" },
+  { img: require("../img/vin-nature.png"), ratio: 1, url: "project.html" },
+  { img: require("../img/nude-ocean.jpg"), ratio: 0.66, url: "project.html" },
+  { img: require("../img/egypte.jpg"), ratio: 0.66, url: "project.html" },
+  { img: require("../img/all-times-1.jpeg"), ratio: 1.24, url: "project.html" },
 ];
 
-var container = document.querySelector(".scene-container");
+var container = document.querySelector(".l-scene-container");
 var width = container.clientWidth;
 var height = container.clientHeight;
 
@@ -29,15 +35,21 @@ var textureLoader = new THREE.TextureLoader();
 var group = new THREE.Group();
 
 // Create and Load Textures for Cubes
+var cubes = [];
+var numCubes = 10; // Adjust based on viewport size
+var spacing = 2.7; // Distance between cubes
+
 projects.forEach((project, index) => {
   var texture = textureLoader.load(project.img);
-  var width = 3;
-  var height = 3 * project.ratio;
-  var geometry = new THREE.BoxGeometry(width, height, 0.02);
+  var width = 4.5;
+  var height = 4.5 * project.ratio;
+  var geometry = new THREE.BoxGeometry(width, height, 0.08);
   var material = new THREE.MeshBasicMaterial({ map: texture });
   var cube = new THREE.Mesh(geometry, material);
-  cube.position.z = index * 3;
+
+  cube.position.z = index * spacing;
   group.add(cube);
+  cubes.push(cube);
 });
 
 //Group
@@ -48,10 +60,17 @@ scene.add(group);
 
 //Scroll Interaction
 window.addEventListener("wheel", (event) => {
-  var delta = event.deltaY;
-
-  collageCube.position.z += delta * 0.002;
-  bleueCube.position.z += delta * 0.002;
+  var delta = event.deltaY * 0.002;
+  cubes.forEach((cube) => {
+    cube.position.z += delta;
+    // If the cube moves too far back, recycle it to the front
+    if (cube.position.z > numCubes * spacing) {
+      cube.position.z -= numCubes * spacing;
+    }
+    if (cube.position.z < -spacing) {
+      cube.position.z += numCubes * spacing;
+    }
+  });
 });
 
 //Animate Scene
@@ -62,9 +81,9 @@ function animate() {
 
 animate();
 
-// var headerLanguages = document.querySelector(".header-languages");
-// var language = document.querySelector(".window");
-// var toggleMenu = function toggleMenu() {
-//   language.classList.toggle("is-active");
-// };
-// headerLanguages.addEventListener("click", toggleMenu);
+var headerLanguages = document.querySelector(".header-languages");
+var language = document.querySelector(".window");
+var toggleMenu = function toggleMenu() {
+  language.classList.toggle("is-active");
+};
+headerLanguages.addEventListener("click", toggleMenu);
