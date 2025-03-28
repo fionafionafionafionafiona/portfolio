9a56e995608e3fccd86f11502591d2f1a3f5c86a
fionaperina <fiona.perina@studentfr.ch>
2025-03-28 16:38:37 +0100

    scroll des projets en 3D réussi correctement :)

--- src/js/main.js
+++ src/js/main.js
@@ -9,11 +9,11 @@ var projects = [
   { img: require("../img/bleue.png"), ratio: 0.98, url: "project.html" },
   { img: require("../img/forest.jpg"), ratio: 0.6, url: "project.html" },
   { img: require("../img/vitamin-well.png"), ratio: 0.6, url: "project.html" },
-  { img: require("../img/qrace.png"), ratio: 0.79, url: "project.html" },
   { img: require("../img/vin-nature.png"), ratio: 1, url: "project.html" },
   { img: require("../img/nude-ocean.jpg"), ratio: 0.66, url: "project.html" },
   { img: require("../img/egypte.jpg"), ratio: 0.66, url: "project.html" },
   { img: require("../img/all-times-1.jpeg"), ratio: 1.24, url: "project.html" },
+  { img: require("../img/obsucra-2.png"), ratio: 0.62, url: "project.html" },
 ];
 
 var container = document.querySelector(".l-scene-container");
@@ -36,10 +36,25 @@ var group = new THREE.Group();
 
 // Create and Load Textures for Cubes
 var cubes = [];
-var numCubes = 10; // Adjust based on viewport size
+// var numCubes = 10; // Adjust based on viewport size
 var spacing = 2.7; // Distance between cubes
+var numCubes = Math.ceil(window.innerHeight / spacing) + 5;
+var initialOffset = -(numCubes * spacing) / 2 + spacing;
 
-projects.forEach((project, index) => {
+// projects.forEach((project, index) => {
+//   var texture = textureLoader.load(project.img);
+//   var width = 4.5;
+//   var height = 4.5 * project.ratio;
+//   var geometry = new THREE.BoxGeometry(width, height, 0.08);
+//   var material = new THREE.MeshBasicMaterial({ map: texture });
+//   var cube = new THREE.Mesh(geometry, material);
+
+//   cube.position.z = i * spacing + initialOffset;
+//   group.add(cube);
+//   cubes.push(cube);
+// });
+for (var i = 0; i < numCubes; i++) {
+  var project = projects[i % projects.length]; // Loop through images
   var texture = textureLoader.load(project.img);
   var width = 4.5;
   var height = 4.5 * project.ratio;
@@ -47,10 +62,12 @@ projects.forEach((project, index) => {
   var material = new THREE.MeshBasicMaterial({ map: texture });
   var cube = new THREE.Mesh(geometry, material);
 
-  cube.position.z = index * spacing;
+  // Adjust the initial position to start from the top
+  cube.position.z = i * spacing + initialOffset;
+
   group.add(cube);
   cubes.push(cube);
-});
+}
 
 //Group
 group.rotation.y = (Math.PI / 6) * -1;
@@ -63,16 +80,23 @@ window.addEventListener("wheel", (event) => {
   var delta = event.deltaY * 0.002;
   cubes.forEach((cube) => {
     cube.position.z += delta;
-    // If the cube moves too far back, recycle it to the front
-    if (cube.position.z > numCubes * spacing) {
-      cube.position.z -= numCubes * spacing;
-    }
-    if (cube.position.z < -spacing) {
-      cube.position.z += numCubes * spacing;
-    }
   });
-});
+  // If the cube moves too far back, recycle it to the front
+  var firstCube = cubes[0];
+  var lastCube = cubes[cubes.length - 1];
 
+  if (firstCube.position.z > spacing * 2) {
+    var movedCube = cubes.shift(); // Remove first cube
+    movedCube.position.z = lastCube.position.z - spacing; // Move it behind the last one
+    cubes.push(movedCube); // Add it back at the end
+  }
+
+  if (lastCube.position.z < -spacing * numCubes) {
+    var movedCube = cubes.pop(); // Remove last cube
+    movedCube.position.z = firstCube.position.z + spacing; // Move it to the front
+    cubes.unshift(movedCube); // Add it back at the start
+  }
+});
 //Animate Scene
 function animate() {
   requestAnimationFrame(animate);
